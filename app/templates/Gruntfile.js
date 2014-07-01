49b28a87f781e8380aaec33d69eea833bf912203
周培公 <peigong@foxmail.com>
2014-07-01 15:24:34 +0800

    app/templates/Gruntfile.js

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
           { expand: true, cwd: "bower_components/node-server/lib/config", src: ['package.json'], dest: 'dist/server' },
           { expand: true, cwd: "bower_components/node-server/lib/config", src: ['log4js.json'], dest: 'dist/config' },
           { expand: true, cwd: "src/bin", src: ['**'], dest: 'dist/bin' },
-          { expand: true, cwd: "src/config", src: ['stub.json'], dest: 'dist/config' },
+          { expand: true, cwd: "src/config", src: ['**'], dest: 'dist/config' },
           { expand: true, cwd: "lib", src: ['**'], dest: 'dist/server' }
         ]
       }
